012b2e3e05214be99081c6a6d4f04c6b58c4346f
Ankur Dutta <ankur@shipthis.co>
2021-08-18 16:47:59 +0530

    First commit

---
+++ src/app/components/cards/card-page-visits/card-page-visits.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-page-visits',
+  templateUrl: './card-page-visits.component.html',
+})
+export class CardPageVisitsComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/components/cards/card-social-traffic/card-social-traffic.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-social-traffic',
+  templateUrl: './card-social-traffic.component.html',
+})
+export class CardSocialTrafficComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
